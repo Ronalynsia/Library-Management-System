84f0bd4a79cad854f0bd9190559979c2d601b247
Ronalynsia <2022315144@dhvsu.edu.ph>
2024-11-26 11:30:47 +0800

    update

--- admin/borrow.php
+++ admin/borrow.php
@@ -44,6 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add_borrow'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Borrow Transactions</title>
+    <link rel="stylesheet" href="css/borrow.css">
     <style>
         table {
             width: 100%;
