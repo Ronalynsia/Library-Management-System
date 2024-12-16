58349bd3f50ae2b89020778c51a2e87c4581ec46
Dylan <dylanmercado0311@gmail.com>
2024-12-16 21:50:35 +0800

    update student-list.php

--- admin/student-list.php
+++ admin/student-list.php
@@ -182,7 +182,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['delete_student'])) {
         fetch(`fetch-student.php?id=${studentId}`)
         .then(response => response.json())
         .then(data => {
-            document.getElementById('edit-id').value = data.id;
+            document.getElementById('edit-id').value = data.student_id;
             document.getElementById('edit-first-name').value = data.first_name;
             document.getElementById('edit-last-name').value = data.last_name;
             document.getElementById('edit-course').value = data.course_id;
